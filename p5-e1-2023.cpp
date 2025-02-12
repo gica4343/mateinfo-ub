5d93375f610b01bdfe6326163729ba82a7ba368a
gica <gica4343@tutanota.com>
2025-02-12 08:19:05 +0200

    +

---
+++ p5-e1-2023.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int f = 30;
+    int f1 = 2;
+    int f2 = 4;
+    int f3 = 12;
+    int f4 = 12;
+    int i = f1 + f2 / 2 + f3 / 3 + f4 / 4;
+    cout << i;
+}
