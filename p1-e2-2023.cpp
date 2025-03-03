72e101631dce67702a7878acde79deb70396752e
gica <gica4343@tutanota.com>
2025-03-03 11:33:20 +0200

    -

--- p1-e2-2023.cpp
+++ p1-e2-2023.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-    
+
     vector<int> p(n);
     for (int i = 0; i < n; i++) {
         cin >> p[i];
@@ -15,18 +15,21 @@ int main() {
     for (int q = 0; q < m; q++) {
         int a, b;
         cin >> a >> b;
-        int test = 0;
-        for (int k = a; k <= b; k++) {
-            test += p[k];
-        }
-        int max_i = 0;
-        for (const auto& it : p) {
-            max_i = max(max_i, it);
-        }
-        if(abs(test - max_i - max_i) == 0) {
+        
+        bool an = false;
+        if (a == b == 0) {
+            an = true;
             cout << "DA" << '\n';
         } else {
-            cout << "NU" << '\n';
+            for (int i = a; i <= b && !an; i++) {
+                for (int j = a; j <= b; j++) {
+                    if (i != j && p[i] >= p[j] && p[i] <= 2 * p[j]) {
+                        an = true;
+                        break;
+                    }
+                }
+            }
+            cout << (an ? "DA" : "NU") << '\n';
         }
     }
 }
