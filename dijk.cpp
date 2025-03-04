f49253666b58cf7db9429b6d1a6059d2da7860e6
gica <gica4343@tutanota.com>
2025-03-04 12:53:09 +0200

    a

--- dijk.cpp
+++ dijk.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -6,20 +7,42 @@
 using namespace std;
 
 int main() {
-    int v = 7;
-    int e = 11;
-    unordered_map<char, vector<pair<char, int>>> adj;
-    for (int i = 0; i < e; i++) {
-        char a, b;
-        int c;
+    int n = 5;
+    vector<vector<pair<int, int>>> graph(n + 1);
+    for (int i = 1; i <= n; i++) {
+        int a, b, c;
         cin >> a >> b >> c;
-        adj[a].push_back({b, c});
+        graph[a].push_back({b, c});
+        graph[b].push_back({a, c});
     }
-    for (const auto& [node, neighbors] : adj) {
-        cout << "Node " << node << " -> ";
-        for (const auto& [neighbor, weight] : neighbors) {
-            cout << "(" << neighbor << ", " << weight << ") ";
+    vector<long long> cost(n + 1, (1LL << 60));
+    vector<int> vis(n + 1);
+    cost[1] = 0;
+
+    priority_queue<pair<long long, int>> s;
+    s.push({0, 1});
+
+    while (!s.empty()) {
+        pair<long long, int> smallest = s.top();
+        s.pop();
+
+        if (vis[smallest.second] == 1) {
+            continue;
         }
-        cout << '\n';
+        vis[smallest.second] = 1;
+
+        for (int i = 0; i < (int)graph[smallest.second].size(); i++) {
+            int next = graph[smallest.second][i].first;
+            int val = graph[smallest.second][i].second;
+
+            if (-smallest.first + val < cost[next]) {
+                cost[next] = -smallest.first + val;
+                s.push({-cost[next], next});
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << cost[i] << ' ';
     }
 }
