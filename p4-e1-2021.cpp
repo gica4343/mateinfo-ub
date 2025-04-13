b6d2cbcebfcc10fbdb3f0d115a8c0fad30e45462
gica <gica4343@tutanota.com>
2025-04-13 13:02:07 +0300

    +

---
+++ p4-e1-2021.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    // A. 4801
+    // B. 4802
+    // C. 4803
+    // D. 6790
+    // E. 6791
+    
+
+    int m = 23052021;
+    cout << (6791 * (6791 - 1) / 2);
+}
