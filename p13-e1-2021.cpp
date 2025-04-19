28debfae67b14fb61e251e0ab18fe99a8943f44c
gica <gica4343@tutanota.com>
2025-04-19 08:31:34 +0300

    +

--- p13-e1-2021.cpp
+++ p13-e1-2021.cpp
@@ -3,19 +3,31 @@
 
 using namespace std;
 
-int main() {
-    vector<vector<int>> v;
-
-    int i = 1;
-    int j = 5;
-    int nr = 1;
-    while (i <= j) {
-        vector<int> x;
-        for (int k = 1; k <= i; i++) {
-            x.push_back(nr);
-            nr++;
-        }
-        v.push_back(x);
-        i++;
+long long sum(vector<int> &v) {
+    if (v.size() < 3) {
+        return 0;
     }
+    long long res = 0;
+    for (int i = 1; i <= v.size() - 2; i++) {
+        res += v[i];
+    }
+    return res;
 }
+
+int main() {
+    long long s = 0;
+    int k = 1;
+    int nr = 1;
+    for (int i = 1; i <= 2020; i++) {
+        vector<int> u;
+        for (int j = 1; j <= nr; j++) {
+            u.push_back(k);
+            k++;
+        }
+        s += sum(u);
+        nr++;
+    }
+
+    cout << s; // B.
+}
+
